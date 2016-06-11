d0fe58e40c8761f6374f3c73a02ff686e8a50da3
chikalaka <edan.chetrit@gmail.com>
2016-06-11 07:50:26 +0300

    how to play done (no css!)

--- client/app/howtoplay/howtoplay.controller.js
+++ client/app/howtoplay/howtoplay.controller.js
@@ -10,9 +10,21 @@ angular.module('whoSaidApp')
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [
-        {id: 0, image: 'assets/images/download.jpg', text: 'slide doron'},
-        {id: 1, image: 'assets/images/download (1).jpg', text: 'slide 2'},
-        {id: 2, image: 'assets/images/download (2).jpg', text: 'slide 3'}
+        {id: 0, image: 'assets/images/download.jpg',
+         p1: '1. Create groups containing your circle of friends.',
+         p2: '2. Post a sentence that you used to hear your friends say to one another.',
+         p3: '3. Don\'t forget to post \"Who Said\" to \"Whom\" !'
+        },
+        {id: 1, image: 'assets/images/download (1).jpg', 
+         p1: 'heart? maybe add directly to picture',
+         p2: '4. Get extra points for posting a sentence.',
+         p3: '5. Wait for your friends to start guessig.'
+        },
+        {id: 2, image: 'assets/images/download (2).jpg', 
+         p1: 'glass?',
+         p2: '6. Go throught your wall and try guessing your friends posts to score more points.',
+         p3: 'button'
+        }
     ];
     
 });
